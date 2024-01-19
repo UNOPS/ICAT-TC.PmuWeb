e772de701310e727ccd20fc8a8951cdfe899df1e
Dilanka Ekanayake <dilanka@climatesi.com>
2024-01-19 10:02:14 +0530

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,14 @@ export const environment = {
   // baseUrlAPI: 'http://localhost:7081',
   // baseSyncAPI: 'http://localhost:7090',
   // baseUrlLandingPage : 'http://localhost/4200/landing-page',
-  baseUrlAPIDocUploadAPI: 'http://localhost:7081/document/upload2',
-  baseUrlAPIDocdownloadAPI: 'http://localhost:7081/document/downloadDocument',
-  baseUrlCountryAPI: 'http://localhost:7081',
-  baseUrlAuditlog :  'http://65.0.7.85:7000',
-  baseUrlAPI: 'http://localhost:7081',
-  baseSyncAPI: 'http://localhost:7090',
-  baseMainSyncAPI: 'https://icat-tc-tool.climatesi.com/web-api',
+  baseUrlAPI: 'http://15.206.202.183:7081',
+  baseUrlAPIDocUploadAPI: 'http://15.206.202.183:7081/document/upload2',
+  baseUrlAPIDocdownloadAPI: 'http://15.206.202.183:7081/document/downloadDocument',
+  baseUrlCountryAPI: 'http://15.206.202.183:7081',
+  baseUrlAuditlog :  'http://15.206.202.183:7000',
+
+  baseSyncAPI: 'http://15.206.202.183:3001',
+  baseMainSyncAPI: 'http://15.206.202.183/web-api',
   baseUrlLandingPage : 'http://15.206.202.183/country/landing-page',
 
 };
