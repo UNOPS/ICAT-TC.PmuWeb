5f744339a3820c39ab5ad4c9aca3e3b3951fe3d8
Dilanka Ekanayake <dilanka@climatesi.com>
2024-01-23 16:27:41 +0530

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
   production: true,
+   // baseUrlAPI: 'http://35.154.205.109:7081',
+  // baseUrlAPIDocUploadAPI: 'http://35.154.205.109:7081/document/upload2',
+  // baseUrlAPIDocdownloadAPI: 'http://15.206.202.183:7081/document/downloadDocument',
+  // baseUrlCountryAPI: 'http://15.206.202.183:7081',
+
+  baseSyncAPI: 'http://3.108.9.184:7090',
+  baseUrlAPI: 'https://52.66.203.70/web-api',
+  esbaseUrlAPI: 'https://15.206.202.183:7090',
+  authBaseUrlAPI: 'https://52.66.203.70/auth-api',
+  baseUrlAPIDocUploadAPI: 'https://52.66.203.70/web-api/document/upload2',
+  
+  baseUrlAuditlog :  'https://52.66.203.70/audit-api',
+  baseUrlJsonFile: 'https://15.206.202.183:3600/filename',
+  baseUrlAPIDocUploadAnonymousAPI: 'https://52.66.203.70/web-api/document/upload2',
+  baseUrlExcelUpload:  'https://52.66.203.70/web-api/parameter/upload',
+  baseUrlPMU: 'https://52.66.203.70/pmu/login',
+  videoURL: 'https://52.66.203.70/web-api/video/'
 };
