34338a099f50856c3fa8b2b13a2421b6e2f38289
Dilanka Ekanayake <dilanka@climatesi.com>
2024-01-23 17:23:10 +0530

    es2022

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,16 +5,22 @@ export const environment = {
   // baseUrlAPIDocdownloadAPI: 'http://15.206.202.183:7081/document/downloadDocument',
   // baseUrlCountryAPI: 'http://15.206.202.183:7081',
 
-  baseSyncAPI: 'http://3.108.9.184:7090',
-  baseUrlAPI: 'https://52.66.203.70/web-api',
+  // baseSyncAPI: 'http://3.108.9.184:7090',
+  // baseUrlAPI: 'https://52.66.203.70/web-api',
   esbaseUrlAPI: 'https://15.206.202.183:7090',
   authBaseUrlAPI: 'https://52.66.203.70/auth-api',
   baseUrlAPIDocUploadAPI: 'https://52.66.203.70/web-api/document/upload2',
   
-  baseUrlAuditlog :  'https://52.66.203.70/audit-api',
+  // baseUrlAuditlog :  'https://52.66.203.70/audit-api',
   baseUrlJsonFile: 'https://15.206.202.183:3600/filename',
   baseUrlAPIDocUploadAnonymousAPI: 'https://52.66.203.70/web-api/document/upload2',
   baseUrlExcelUpload:  'https://52.66.203.70/web-api/parameter/upload',
   baseUrlPMU: 'https://52.66.203.70/pmu/login',
-  videoURL: 'https://52.66.203.70/web-api/video/'
+  videoURL: 'https://52.66.203.70/web-api/video/',
+
+  baseUrlCountryAPI: 'https://52.66.203.70/web-api',
+  baseUrlAuditlog :  'https://52.66.203.70/audit-api',
+  baseUrlAPI: 'https://52.66.203.70/web-api',
+  baseSyncAPI: 'https://52.66.203.70/auth-api',
+  baseUrlLandingPage : 'http://localhost/4200/landing-page',
 };
